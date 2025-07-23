b61b9ad1251f8cb3429bd429796f646ba7dd16f9
Lukasz <lukaszdybzinski@gmail.com>
2025-07-23 16:11:30 +0200

    final version

--- main.py
+++ main.py
@@ -10,4 +10,19 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
+password_list = []
 
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+password= ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
